123bcbb2738de3c51fb5318a0e179ac38a6fd45f
ArkarPhyoe <arkarphyoe3396@gmail.com>
2020-08-05 16:17:23 +0630

    part_5_database_and_migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
-
-## dummy posts 
-
-posts = [
-    {'author':'admin', 'title':'Post 1', 'content':'First Post' ,'date_posted':'August 3, 2020'},
-    {'author': 'testuser', 'title':'Post 2', 'content':'Second Post', 'date_posted':'August 4, 2020'}
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
-    context = {'posts': posts }
+    context = {'posts': Post.objects.all() }
     return render(request, 'blog/home.html', context)
 
 def about(request):
